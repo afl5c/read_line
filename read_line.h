de242089f9655936e6be6c5cd1d06636c6e592aa
Aaron <aaron@aaron-air.local>
2021-04-07 23:15:35 -0700

    fix compatibility with lldb and pasting multiple lines of text (in some cases)

--- read_line.h
+++ read_line.h
@@ -75,7 +75,8 @@ string read_line(const string& prompt="> "){
 		if(c==1) pos = 0; //ctrl-a (go to line start)
 		else if(c==3) raise(SIGINT); //ctrl-c
 		else if(c==5) pos = line.size(); //ctrl-e (go to line end)
-		else if(c==13) break; //enter
+		else if(c==10) break; //enter (newline)
+		else if(c==13) break; //enter (carriage return)
 		else if(c==26){ //ctrl-z
 			raise(SIGTSTP);
 			tcsetattr(STDIN_FILENO,TCSANOW,&raw); //must re-set raw mode
